6459cbc3c478994e8c29b1ec51770b6e1b8c0baa
solomon jesse <statosco@gmail.com>
2023-11-29 07:29:51 +0300

    initial commit

---
+++ HPC/stl.cpp
@@ -0,0 +1,7 @@
+// standard tmplate lib is a set of template
+// classes that provide many basic algorithms 
+// or containers
+
+//container > objects that are used to store (element) or similar types eg. array, vectors etc
+
+//iterators > 
