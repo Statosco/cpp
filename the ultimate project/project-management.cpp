9547fcba0abf5c46a296ab93b7011f2042058c76
solomon jesse <statosco@gmail.com>
2024-01-10 08:02:58 +0300

    initial commit

--- the ultimate project/project-management.cpp
+++ the ultimate project/project-management.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 #include <algorithm>  // for std::transform
 #include <cctype>     // for std::tolower
 
 class Task{
     private:
-    void admin(){};
-    void newUser(){};
+    void admin(string name, string email, string password){};
+    void newUser(string name, string email, string password){};
 
     public:
-    void loginUser(){
+    string loginUser(){
         cout<<"\n\n\t\t\t---------------------LOGIN PAGE---------------------\n";
+        string code;
         
+        cout<<"\n\n______For admins panell type in your admin code or press any character to prossed to a local account\n";
+        cin>>code;
 
     }
     void registerUser(){
@@ -22,12 +26,13 @@ class Task{
 
 };
 
+
 void mainMenu(){
     cout<<"\n\n\t\t\t---------------------M_A_I_N--M_E_N_U---------------------\n";
 
     Task T;
-    int options;
-
+    char options;
+    n:
     cout<<"_____Do you have an account with us? (n/y) -->> ";
     cin>>options;
 
@@ -42,12 +47,31 @@ void mainMenu(){
         break;
     
     default:
+        cout<< "you have inserted the wrong option please try again\n";
+        goto n;
         break;
     };
 
 }
 
+string secretKey(){
+
+}
+
+void adminCode(){
+    fstream file;
+
+    ofstream file("secrets.txt");
+
+    if(file.is_open()){
+        file << secretKey();
+
+        file.close();
+    };
+}
+
 int main(){
     cout<<"\n\n\t\t\t---------------------W E L C O M E---------------------\n";
+    adminCode();
     mainMenu();
 }
